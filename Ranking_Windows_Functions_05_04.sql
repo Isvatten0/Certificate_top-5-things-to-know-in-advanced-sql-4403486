e86d828d0d453ce1d96bbf23266642d5ca9e9579
Luke Roberts <104106765+Isvatten0@users.noreply.github.com>
2025-03-19 14:33:00 -0400

    Update Ranking_Windows_Functions_05_04.sql

--- Ranking_Windows_Functions_05_04.sql
+++ Ranking_Windows_Functions_05_04.sql
@@ -4,26 +4,9 @@ Challenge Solution: Ranking Windows Functions
 Task: Write a query using RANK() or DENSE_RANK() that pulls all registration information for the
 first three people that registered for the Red30Tech Conference in each state.
 */
-
--- Preview data if necessary
--- SELECT * FROM [Red30Tech].[dbo].[ConventionAttendees$]
-
---- SOLUTION WITH DENSE RANK
--- Notice solution has 229 rows!
-WITH RANKS AS (
+WITH Ranking AS (
 	SELECT * ,
-	DENSE_RANK() OVER (PARTITION BY [State] ORDER BY [Registration Date]) as DENSE_RANK_
+	RANK() OVER (PARTITION BY [State] ORDER BY [Registration Date]) as Top
 	FROM [Red30Tech].[dbo].[ConventionAttendees$]
-	)
-
-SELECT * FROM RANKS WHERE DENSE_RANK_ in (1,2,3)
-
---- SOLUTION WITH RANK
--- Notice solution has 171 rows!
---WITH RANKS AS (
---	SELECT * ,
---	RANK() OVER (PARTITION BY [State] ORDER BY [Registration Date]) as RANK_
---	FROM [Red30Tech].[dbo].[ConventionAttendees$]
---	)
-
---SELECT * FROM RANKS WHERE RANK_ in (1,2,3)
+)
+SELECT * FROM RANKING WHERE Top in (1,2,3)
