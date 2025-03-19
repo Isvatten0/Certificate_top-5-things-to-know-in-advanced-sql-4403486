807cf49e6893bf8ed546cdd1c3143e5e778a6de7
Luke Roberts <104106765+Isvatten0@users.noreply.github.com>
2025-03-19 14:21:56 -0400

    Changed to my solution

--- Row_Number_03_04.sql
+++ Row_Number_03_04.sql
@@ -1,5 +1,6 @@
 /*
 Created By: Kendall Ruber
+Modified by: Luke Roberts
 Challenge Solution: ROW_NUMBER()
 Task: Write a query using ROW_NUMBER() that returns the OrderNum, OrderDate, CustName, ProdCategory, ProdName, and 
 Order Total of the top 3 orders that have the highest Order Total from each ProdCategory purchased by Boehm Inc.
@@ -9,13 +10,23 @@ Order Total of the top 3 orders that have the highest Order Total from each Prod
 -- SELECT * FROM [Red30Tech].[dbo].[OnlineRetailSales$]
 
 -- Solution: Remember your solution may look slightly different than mine! You could also use a subquery to accomplish this.
-WITH ROW_NUMBERS as (
-					SELECT OrderNum, OrderDate, CustName, ProdCategory, ProdName, [Order Total],
-					ROW_NUMBER() OVER(PARTITION BY [ProdCategory] ORDER BY [Order Total] DESC) as ROW_NUM
-					FROM  [Red30Tech].[dbo].[OnlineRetailSales$]
-					where CustName = 'Boehm Inc.'
-					)
+WITH TopOrders as (
+	SELECT  OrderNum,
+	        OrderDate,
+		CustName, 
+		ProdCategory, 
+		ProdName, 
+		[Order Total],
+	ROW_NUMBER() OVER(PARTITION BY [ProdCategory] ORDER BY [Order Total] DESC) as ORDERS
+	FROM  [Red30Tech].[dbo].[OnlineRetailSales$]
+	WHERE CustName = 'Boehm Inc.'
+)
 
-SELECT OrderNum, OrderDate, CustName, ProdCategory, ProdName, [Order Total] FROM ROW_NUMBERS 
-WHERE ROW_NUM in (1,2,3)
+SELECT  OrderNum,
+	OrderDate,
+	CustName,
+	ProdCategory,
+	ProdName,
+	[Order Total] FROM TopOrders 
+WHERE ORDERS in (1,2,3)
 ORDER BY [ProdCategory],[Order Total] DESC
